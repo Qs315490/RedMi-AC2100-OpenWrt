3dd61da073143e534029a22a59653b163dd6ed46
Qs315490 <1811138747@qq.com>
2021-07-02 08:19:55 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,5 +12,9 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generate
+# 修改设备名称
+sed -i 's/OpenWrt/HMAC2100/g' package/base-files/files/bin/config_generate
+# 修改时区
+sed -i 's\UTC\Asia/Shanghai\g' package/base-files/files/bin/config_generate
 # 修改 argon 为默认主题,可根据你喜欢的修改成其他的（不选择那些会自动改变为默认主题的主题才有效果）
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
