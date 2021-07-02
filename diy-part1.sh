c6d799b62f9b0dae4e6559d85596d80c460140a8
Qs315490 <1811138747@qq.com>
2021-07-02 14:41:34 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,16 +15,7 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-#sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git custom https://github.com/garypang13/openwrt-packages.git' feeds.conf.default
 
 # 添加软件包源
 git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon  #新的argon主题
-git clone --depth=1 https://github.com/mchome/openwrt-vlmcsd.git package/vlmcsd
-git clone --depth=1 https://github.com/mchome/luci-app-vlmcsd.git package/luci-app-vlmcsd
-git clone --depth=1 https://github.com/garypang13/luci-app-dnsfilter.git package/luci-app-dnsfilter
-git clone --depth=1 https://github.com/sirpdboy/luci-app-advanced.git package/luci-app-advanced
-#git clone --depth=1 https://github.com/tianiue/luci-app-bypass.git package/luci-app-bypass
-git clone --depth=1 https://github.com/immortalwrt/luci-app-unblockneteasemusic-mini.git package/luci-app-unblockneteasemusic-mini
-git clone --depth=1 https://github.com/awesome-openwrt/luci-app-arpbind.git package/luci-app-arpbind
-git clone --depth=1 https://github.com/deplives/luci-app-turboacc.git package/luci-app-turboacc
-git clone --depth=1 https://github.com/awesome-openwrt/pdnsd-alt.git package/pdnsd-alt
