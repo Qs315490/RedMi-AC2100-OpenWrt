48062a2aa20a50f342959df89ece2ddb5368a142
Qs315490 <1811138747@qq.com>
2021-08-09 10:07:56 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -19,7 +19,3 @@ sed -i 's/UTC/CST-8/g' package/base-files/files/bin/config_generate
 sed -i "/timezone/a\\\t\tset system.@system[-1].zonename='Asia\/Shanghai'" package/base-files/files/bin/config_generate
 # 修改 argon 为默认主题,可根据你喜欢的修改成其他的（不选择那些会自动改变为默认主题的主题才有效果）
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
-# 设置文件夹权限
-if [ ! -d "files" ];then
-	chmod -R 755 files
-fi
