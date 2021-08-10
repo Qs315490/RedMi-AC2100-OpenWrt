39ec49447c3d3cc5275769907b13fcce4bfe0f14
Qs315490 <1811138747@qq.com>
2021-08-10 11:03:26 +0800

    Update OpenWrt-part1.sh

--- OpenWrt-part1.sh
+++ OpenWrt-part1.sh
@@ -19,4 +19,10 @@ sed -i '$a src-git custom https://github.com/kiddin9/openwrt-packages.git' feeds
 
 # 添加软件包源
 git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon  #新的argon主题
-#git clone --depth=1 https://github.com/LGA1150/openwrt-fullconenat package/fullconenat  #全锥形Nat
+
+git clone --depth=1 https://github.com/LGA1150/openwrt-fullconenat package/fullconenat  #全锥形Nat
+mkdir package/network/config/firewall/patches
+wget -P package/network/config/firewall/patches/ https://github.com/LGA1150/fullconenat-fw3-patch/raw/master/fullconenat.patch
+pushd feeds/luci
+wget -O- https://github.com/LGA1150/fullconenat-fw3-patch/raw/master/luci.patch | git apply
+popd
