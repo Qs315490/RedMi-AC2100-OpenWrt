9fb48743c9b083f08de53ce0302e79ae8c3eef80
Qs315490 <1811138747@qq.com>
2021-08-28 22:21:12 +0800

    Update OpenWrt-part2.sh

--- OpenWrt-part2.sh
+++ OpenWrt-part2.sh
@@ -19,3 +19,7 @@ sed -i 's/UTC/CST-8/g' package/base-files/files/bin/config_generate
 sed -i "/timezone/a\\\t\tset system.@system[-1].zonename='Asia\/Shanghai'" package/base-files/files/bin/config_generate
 # 修改 argon 为默认主题,可根据你喜欢的修改成其他的（不选择那些会自动改变为默认主题的主题才有效果）
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+
+# 报错修复
+cp /usr/bin/upx /workdir/openwrt/staging_dir/host/bin/
+cp /usr/bin/upx-ucl /workdir/openwrt/staging_dir/host/bin/
