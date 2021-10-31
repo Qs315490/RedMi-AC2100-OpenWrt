cdf122412381a3f8a71b891b01f296090c60ce5b
Qs315490 <1811138747@qq.com>
2021-10-31 14:01:57 +0800

    Update OpenWrt-part2.sh

--- OpenWrt-part2.sh
+++ OpenWrt-part2.sh
@@ -10,15 +10,16 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
+# 以下配置已改为固件内的defsetting服务进行设置
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generate
 # 修改设备名称
-sed -i 's/OpenWrt/HMAC2100/g' package/base-files/files/bin/config_generate
+#sed -i 's/OpenWrt/HMAC2100/g' package/base-files/files/bin/config_generate
 # 修改时区(貌似没有起作用)
-sed -i 's/UTC/CST-8/g' package/base-files/files/bin/config_generate
-sed -i "/timezone/a\\\t\tset system.@system[-1].zonename='Asia\/Shanghai'" package/base-files/files/bin/config_generate
+#sed -i 's/UTC/CST-8/g' package/base-files/files/bin/config_generate
+#sed -i "/timezone/a\\\t\tset system.@system[-1].zonename='Asia\/Shanghai'" package/base-files/files/bin/config_generate
 # 修改 argon 为默认主题,可根据你喜欢的修改成其他的（不选择那些会自动改变为默认主题的主题才有效果）
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # 报错修复
 cp /usr/bin/upx /workdir/openwrt/staging_dir/host/bin/
